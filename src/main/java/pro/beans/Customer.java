ab0638b6a3794dd7b26cd1e54dd3c79c02b7bf8a
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2021-07-15 19:33:29 +0300

    project configuration

--- src/main/java/pro/beans/Customer.java
+++ src/main/java/pro/beans/Customer.java
@@ -1,32 +1,32 @@
-package pro.beans;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import org.springframework.stereotype.Component;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Component
-@Data
-@Entity
-@AllArgsConstructor
-@NoArgsConstructor
-public class Customer {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private int id;
-	
-	private String firstName;
-	
-	private String lastName;
-		
-	private String street;
-	
-	private int idCard;
-		
-
-}
+//package pro.beans;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//import org.springframework.stereotype.Component;
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//
+//@Component
+//@Data
+//@Entity
+//@AllArgsConstructor
+//@NoArgsConstructor
+//public class Customer {
+//	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private int id;
+//	
+//	private String firstName;
+//	
+//	private String lastName;
+//		
+//	private String street;
+//	
+//	private int idCard;
+//		
+//
+//}
