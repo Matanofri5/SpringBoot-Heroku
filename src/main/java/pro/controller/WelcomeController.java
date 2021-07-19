fc5e5335e9cb1aea0560a8384e9faf1a33f2f245
Matanofri5 <45266810+Matanofri5@users.noreply.github.com>
2021-07-19 18:21:58 +0300

    adding 3000 localHost

--- src/main/java/pro/controller/WelcomeController.java
+++ src/main/java/pro/controller/WelcomeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,7 +15,7 @@ import pro.beans.Customer;
 import pro.repository.CustomerRepository;
 import pro.service.impl.CustomerServiceImpl;
 
-
+@CrossOrigin(origins = "https://localhost:3000")
 @RestController
 public class WelcomeController {
 	
